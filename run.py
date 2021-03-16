46c1e47ebe4a71e9405ad0c3f0eb7205a230d170
MannyBinning <manib58@live.co.uk>
2021-03-16 17:29:49 +0000

    Added Profile page and functionality for current user

--- run.py
+++ run.py
@@ -23,11 +23,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
-
-
 @app.route("/services")
 def services():
     return render_template("services.html")
@@ -66,6 +61,8 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -81,7 +78,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -93,7 +93,15 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
-    
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)    
+
 
 if __name__ == "__main__":
     app.run(
