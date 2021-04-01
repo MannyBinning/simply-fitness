be8ef2adc163518572b274ce3e128c1f467841e3
MannyBinning <manib58@live.co.uk>
2021-04-01 22:07:58 +0000

    Added images for subscription leveles

--- run.py
+++ run.py
@@ -140,24 +140,30 @@ def edit_profile(username_id):
             existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
-            if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+            if existing_user:
+                if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
 
-                    update = {
-                        "FirstName": request.form.get("FirstName"),
-                        "LastName": request.form.get("LastName"),
-                        "subscription": request.form.get("subscription"),
-                        "address": request.form.get("address"),
-                        "email": request.form.get("email"),
-                        "username": request.form.get("username").lower(),
-                        "password": generate_password_hash(request.form.get("password"))
-                    }
-                    mongo.db.users.update({"_id": ObjectId(username_id)}, update)
-                    flash("Update Successful!")
+                        update = {
+                            "FirstName": request.form.get("FirstName"),
+                            "LastName": request.form.get("LastName"),
+                            "subscription": request.form.get("subscription"),
+                            "address": request.form.get("address"),
+                            "email": request.form.get("email"),
+                            "username": request.form.get("username").lower(),
+                            "password": generate_password_hash(request.form.get("password"))
+                        }
+                        mongo.db.users.update({"_id": ObjectId(username_id)}, update)
+                        flash("Update Successful!")
 
+                else:
+                    # invalid password match
+                    flash("Incorrect Username/Password, Update not complete")
+            
             else:
-                # invalid password match
-                flash("Incorrect Password, Update not complete")
+                #invalid username
+                flash("Incorrect Username/Password, Update not complete")
 
         user = mongo.db.users.find_one({"_id": ObjectId(username_id)})
         username = mongo.db.users.find_one({"username": session["user"]})
