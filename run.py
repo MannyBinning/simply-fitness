5d6a28224256ad5bc82eab8ad1ba29deda23d018
MannyBinning <manib58@live.co.uk>
2021-04-05 12:06:15 +0000

    updated comments on run.py

--- run.py
+++ run.py
@@ -18,27 +18,28 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-#Username validation for chosen characters.
+
+# Username validation for chosen characters
 def check_name(name):
-    # Allow lowercase and uppercase letters 
+    # Allow lowercase and uppercase letters
     # hyphen, dot, underscore and numbers
     return re.match("[a-zA-Z0-9.-_ ]+$", name)
 
 
-#Password validation for chosen characters only
+# Password validation for chosen characters only
 def check_password(password):
     # Allow lowercase and uppercase letters and numbers,
     # 5-15 characters in length.
     return re.match("^[a-zA-Z0-9]{5,15}$", password)
 
 
-#Route to home page
+# Route to home page
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-#Route to services page
+# Route to services page
 @app.route("/services")
 def services():
     return render_template("services.html")
@@ -47,19 +48,17 @@ def services():
 # Route to Register page and Register Functionality
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST":    
-        # check if characters added for username are valid  
+    if request.method == "POST":
+        # check if characters added for username are valid
         if request.form.get("username") == "" or not check_name(
            request.form.get("username").lower()):
             flash("Username Invalid")
             return redirect(url_for("register"))
-        
-        # check if characters added for password are valid  
+        # check if characters added for password are valid
         if request.form.get("password") == "" or not check_password(
            request.form.get("password")):
             flash("Please enter a valid password.")
             return redirect(url_for("register"))
-        
         # check if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
@@ -75,7 +74,6 @@ def register():
         if existing_email:
             flash("Email already exists")
             return redirect(url_for("register"))
-        
         register = {
             "FirstName": request.form.get("FirstName"),
             "LastName": request.form.get("LastName"),
@@ -95,7 +93,7 @@ def register():
     return render_template("register.html")
 
 
-#Route to Login page
+# Route to Login page
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -107,11 +105,11 @@ def login():
             # ensure hashed password matches user input
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
-                        request.form.get("username")))
-                    return redirect(url_for(
-                        "profile", username=session["user"]))
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -125,67 +123,64 @@ def login():
     return render_template("login.html")
 
 
-#Route to Profile page
+# Route to Profile page
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})
-    
-    if session["user"]:    
-        return render_template("profile.html", username=username)  
-    
+    if session["user"]:
+        return render_template("profile.html", username=username)
     return redirect(url_for("login"))
 
 
-#Route to edit profile
+# Route to edit profile
 @app.route("/edit_profile/<username_id>", methods=["GET", "POST"])
 def edit_profile(username_id):
-        if request.method == "POST":
-            #check if username confirmation is correct 
-            existing_user = mongo.db.users.find_one(
+    if request.method == "POST":
+        # check if username confirmation is correct
+        existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
-            #check if password confirmation is correct
-            if existing_user:
-                if check_password_hash(
-                    existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-
-                        update = {
-                            "FirstName": request.form.get("FirstName"),
-                            "LastName": request.form.get("LastName"),
-                            "subscription": request.form.get("subscription"),
-                            "address": request.form.get("address"),
-                            "email": request.form.get("email"),
-                            "username": request.form.get("username").lower(),
-                            "password": generate_password_hash(request.form.get("password"))
-                        }
-                        mongo.db.users.update({"_id": ObjectId(username_id)}, update)
-                        flash("Update Successful!")
-
-                else:
-                    # invalid password match
-                    flash("Incorrect Username/Password, Update not complete")
-            
+        # check if password confirmation is correct
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                update = {
+                    "FirstName": request.form.get("FirstName"),
+                    "LastName": request.form.get("LastName"),
+                    "subscription": request.form.get("subscription"),
+                    "address": request.form.get("address"),
+                    "email": request.form.get("email"),
+                    "username": request.form.get("username").lower(),
+                    "password": generate_password_hash(
+                        request.form.get("password"))
+                }
+                mongo.db.users.update(
+                    {"_id": ObjectId(username_id)}, update)
+                flash("Update Successful!")
             else:
-                # invalid username
+                # invalid password match
                 flash("Incorrect Username/Password, Update not complete")
+        else:
+            # invalid username
+            flash("Incorrect Username/Password, Update not complete")
 
         user = mongo.db.users.find_one({"_id": ObjectId(username_id)})
         username = mongo.db.users.find_one({"username": session["user"]})
-        return render_template("edit_profile.html", user=user, username=username)
+        return render_template(
+            "edit_profile.html", user=user, username=username)
 
 
-#Route to delete the account 
+# Route to delete the account
 @app.route("/delete_profile/<username_id>", methods=["GET", "POST"])
 def delete_profile(username_id):
     if request.method == "POST":
-         #check if username confirmation is correct 
+        # check if username confirmation is correct
         existing_user = mongo.db.users.find_one(
-        {"username": request.form.get("username").lower()})
-
-         #check if password confirmation is correct 
+            {"username": request.form.get("username").lower()})
+        # check if password confirmation is correct
         if existing_user:
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
@@ -198,7 +193,6 @@ def delete_profile(username_id):
             else:
                 # invalid password match
                 flash("Incorrect Username/Password, Update not complete")
-        
         else:
             # invalid username
             flash("Incorrect Username/Password, Update not complete")
@@ -207,7 +201,7 @@ def delete_profile(username_id):
     return render_template("delete_profile.html", username=username)
 
 
-#Route to Logout
+# Route to Logout
 @app.route("/logout")
 def logout():
     flash("You have been logged out")
@@ -215,7 +209,7 @@ def logout():
     return redirect(url_for("login"))
 
 
-#Route to class bookings
+# Route to class bookings
 @app.route("/book_class", methods=["GET", "POST"])
 def book_class():
     if request.method == "POST":
@@ -230,13 +224,13 @@ def book_class():
         mongo.db.tasks.insert_one(task)
         flash("Class Booked")
         return redirect(url_for("book_class"))
-        
     categories = mongo.db.categories.find().sort("category_name", 1)
     tasks = mongo.db.tasks.find()
-    return render_template("book_class.html", categories=categories, tasks=tasks)
+    return render_template(
+        "book_class.html", categories=categories, tasks=tasks)
 
 
-#route to edit classes that are already booked
+# route to edit classes that are already booked
 @app.route("/edit_class/<task_id>", methods=["GET", "POST"])
 def edit_class(task_id):
     if request.method == "POST":
@@ -255,10 +249,11 @@ def edit_class(task_id):
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
     tasks = mongo.db.tasks.find()
-    return render_template("edit_class.html", task=task, categories=categories, tasks=tasks)
+    return render_template(
+        "edit_class.html", task=task, categories=categories, tasks=tasks)
 
 
-#route to delete classes that are booked
+# route to delete classes that are booked
 @app.route("/delete_class/<task_id>")
 def delete_class(task_id):
     mongo.db.tasks.remove({"_id": ObjectId(task_id)})
